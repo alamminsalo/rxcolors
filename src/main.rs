a42a51737df597ad72f425dc3f59ba5ddc0911b5
Antti Lamminsalo <antti.lamminsalo@gmail.com>
2017-08-22 17:44:26 +0300

    ok

--- src/main.rs
+++ src/main.rs
@@ -4,110 +4,67 @@ extern crate hyper_tls;
 extern crate tokio_core;
 extern crate serde_json;
 
-use futures::{future, Future, Stream, BoxFuture, IntoFuture};
-use hyper::{Method, Request, Client, Chunk, StatusCode};
-use hyper::client::FutureResponse;
-use hyper_tls::{HttpsConnector, HttpsConnecting};
+use futures::{Future, Stream};
+use hyper::{Method, Request, Client, Error};
+use hyper_tls::{HttpsConnector};
 use tokio_core::reactor::Core;
 use serde_json::{Value};
 use std::env;
 use std::str;
-use std::io;
 
 // Struct for JSON calls
-struct GithubAPI {
+struct GithubResource {
     client: Client<HttpsConnector<hyper::client::HttpConnector>>
 }
 
-impl GithubAPI {
+impl GithubResource {
 
-
-
-    // fn that adds needed headers to function
-    fn add_headers(req: &mut Request) {
-        req.headers_mut().set_raw("Accept", "application/vnd.github.v3+json".to_owned());
+    fn get_raw(&self, url: &str) -> Box<Future<Item=String, Error=Error>>
+    {
+        let mut req = Request::new(Method::Get, url.parse().unwrap());
+        req.headers_mut().set_raw("Accept", "text/plain".to_owned());
         req.headers_mut().set_raw("User-Agent", "rxres".to_owned());
+
+        Box::new(self.client.request(req)
+                 .and_then(|res| {
+                     res.body().concat2().and_then(|data| {
+                         // Here final result as json value
+                         Ok(String::new(data))
+                     })
+                 })) 
     }
 
-    fn get<A,B,F>(&self, url: &str) -> futures::AndThen<A,B,F> 
-        where 
-            A: Future<Item = serde_json::Value, Error = io::Error>, 
-            B: IntoFuture,
-            F: FnOnce(A::Item) -> B
-            {
-        let mut req = Request::new(Method::Get, url.parse().unwrap());
-        Self::add_headers(&mut req);
 
-        self.client.request(req)
-            .and_then(|res| {
-            if res.status() == StatusCode::Ok {
-                res.body().concat2().and_then(move |data: Chunk| {
-                    Ok((serde_json::from_slice(&data).unwrap()))
-                });
-            }
-        })
+    fn get(&self, url: &str) -> Box<Future<Item=Value, Error=Error>>
+    {
+        let mut req = Request::new(Method::Get, url.parse().unwrap());
+        req.headers_mut().set_raw("Accept", "application/vnd.github.v3+json".to_owned());
+        req.headers_mut().set_raw("User-Agent", "rxres".to_owned());
+
+        Box::new(self.client.request(req)
+                 .and_then(|res| {
+                     res.body().concat2().and_then(|data| {
+                         // Here final result as json value
+                         Ok(serde_json::from_slice(&data).unwrap())
+                     })
+                 }))
     }
 }
 
 fn print_args() {
     println!("Arguments:");
     println!("list\t\t\tPrints all color theme names");
-    println!("search <query>\t\tSearches for given string");
     println!("<themename>\t\tFetches theme and prints to stdout");
-}
-
-// Fn to request themes
-fn get_themes_and(url: &str, action: &Fn(&serde_json::Value)) {
-    let mut core = Core::new().unwrap();
-    let client = Client::configure().connector(HttpsConnector::new(4, &core.handle()).unwrap()).build(&core.handle());
-
-    let uri = url.parse().unwrap();
-    let mut req = Request::new(Method::Get, uri);
-    req.headers_mut().set_raw("Accept", "application/vnd.github.v3+json".to_owned());
-    req.headers_mut().set_raw("User-Agent", "rxres".to_owned());
-
-    let work = client.request(req)
-        .and_then(move |res| {
-            res.body().concat2().and_then(move |data: Chunk| {
-                let v: Value = serde_json::from_slice(&data).unwrap();
-                for o in v.as_array().unwrap() {
-                    action(o);
-                }
-                Ok(())
-            })
-        });
-
-    core.run(work).unwrap();
-}
-
-fn download_theme(url: &str) {
-    let mut core = Core::new().unwrap();
-    let client = Client::configure().connector(HttpsConnector::new(4, &core.handle()).unwrap()).build(&core.handle());
-
-    let uri = url.parse().unwrap();
-    let mut req = Request::new(Method::Get, uri);
-    req.headers_mut().set_raw("Accept", "text/plain".to_owned());
-    req.headers_mut().set_raw("User-Agent", "rxres".to_owned());
-
-    let work = client.request(req)
-        .and_then(move |res| {
-            res.body().concat2().and_then(move |data: Chunk| {
-                println!("{}", str::from_utf8(&data).unwrap());
-                Ok(())
-            })
-        });
-
-    core.run(work).unwrap();
 }
 
 // Main
 fn main() {
 
     let mut core = Core::new().unwrap();
-    let api = GithubAPI{client: Client::configure().connector(HttpsConnector::new(4, &core.handle()).unwrap()).build(&core.handle())};
+    let api = GithubResource{client: Client::configure().connector(HttpsConnector::new(4, &core.handle()).unwrap()).build(&core.handle())};
 
     // Closure operations
-    let print_theme = |theme: &serde_json::Value| println!("{}", theme["name"].as_str().unwrap());
+    let print_theme = |theme: &Value| println!("{}", theme["name"].as_str().unwrap());
 
     let xcolors_url = "https://api.github.com/repos/tlatsas/xcolors/contents/themes";
     let base64_url = "https://api.github.com/repos/chriskempson/base16-xresources/contents/xresources";
@@ -117,39 +74,40 @@ fn main() {
     it.next();
     while let Some(arg) = it.next() {
         if arg == "list" {
-            get_themes_and(xcolors_url, &print_theme);
-            get_themes_and(base64_url, &print_theme);
-            return;
-        }
-
-        else if arg == "search" {
-            let q = Some(it.next().unwrap().clone()).unwrap();
-
-            get_themes_and(xcolors_url, &|theme: &serde_json::Value| {
-                if theme["name"].as_str().unwrap().contains(&q) {
-                    print_theme(theme);
-                }
-            });
-
-            get_themes_and(base64_url, &move |theme: &serde_json::Value| {
-                if theme["name"].as_str().unwrap().contains(&q) {
-                    print_theme(theme);
-                }
-            });
+            let work = api.get(xcolors_url)
+                .join(api.get(base64_url))
+                .and_then(|(a, b)| {
+                    //Chain iterators and print
+                    for value in a.as_array().unwrap().iter()
+                        .chain(b.as_array().unwrap().iter()) {
+                            print_theme(value);
+                        }
+                    Ok(())
+                });
+            core.run(work).unwrap();
             return;
         }
 
         else {
-            get_themes_and(xcolors_url, &|theme: &serde_json::Value|{
-                if theme["name"].as_str().unwrap().trim() == arg {
-                    download_theme(theme["download_url"].as_str().unwrap());
+            let work = api.get(xcolors_url)
+                .join(api.get(base64_url))
+                .and_then(|(a,b)| {
+                    Ok(a.as_array().unwrap().iter()
+                       .chain(b.as_array().unwrap().iter()).collect::<Vec<Value>>())
+                })
+            .and_then(|values| {
+                for value in values {
+                    if value["name"].as_str().unwrap().trim() == arg {
+                        api.get_raw(value["download_url"].as_str().unwrap())
+                            .and_then(|data| {
+                                println!("{:?}", data);
+                                Ok(data)
+                            })
+                    }
                 }
+                Err(())
             });
-            get_themes_and(base64_url, &|theme: &serde_json::Value|{
-                if theme["name"].as_str().unwrap().trim() == arg {
-                    download_theme(theme["download_url"].as_str().unwrap());
-                }
-            });
+            core.run(work).unwrap();
             return;
         }
     }
